f332a3763693a2bbabe5de5a39dd9e60d68203d2
Solovitska <146887721+Solovitska@users.noreply.github.com>
2024-04-14 01:12:26 +0300

    up

--- src/components/ContactList/Contact/Contact.jsx
+++ src/components/ContactList/Contact/Contact.jsx
@@ -2,14 +2,14 @@ import styles from './Contact.module.css'
 import { IoPersonSharp } from "react-icons/io5"
 import { FaPhoneAlt} from 'react-icons/fa'
 
-export default function Contact({data: {name, number, id}, handleDelete}) {
+export default function Contact({data: {name, number, id}, onDelete}) {
     return (
         <li className={styles.list}>
             <div>
                 <p className={styles.textbox}><IoPersonSharp size='18'/>{name}</p>
                 <p className={styles.textbox}><FaPhoneAlt size='17'/>{number}</p>
             </div>
-            <button type="button" onClick={() => {handleDelete(id)}}>Delete</button>
+            <button type="button" onClick={() => {onDelete(id)}}>Delete</button>
         </li>
     )
 }
